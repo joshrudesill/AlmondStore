1a22fcaa8e00aa8586be950cbe7563bc31412040
Joshua Rudesill <joshrudesill@gmail.com>
2021-12-12 16:20:09 -0600

    Added shop page with basic components, added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
+    </div>
   );
 }
 
