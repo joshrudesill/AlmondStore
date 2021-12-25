82264cccc92346c24bd0bcbbc2f6b991824751b3
Joshua Rudesill <joshrudesill@gmail.com>
2021-12-25 05:31:17 -0600

    configured google auth sign in and some button stuff

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Logo} from '../../assets/binoculars.svg'
+import { ReactComponent as Logo} from '../../assets/binoculars.svg';
+import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo' width={50} height={50} viewBox='0 0 16 16'/>
@@ -11,6 +12,12 @@ const Header = () => (
             <div className='options'>
                 <Link className='option' to='/shop'>Shop</Link>
                 <Link className='option' to='/contact'>Contact</Link>
+                {
+                    currentUser ? 
+                    <div className='option' onClick={() => auth.signOut()}>Sign Out</div> 
+                    :
+                    <Link className='option' to='/signin'>Sign In</Link> 
+                }
             </div>
     </div>
 )
--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -46,8 +46,10 @@ class SignIn extends React.Component {
                          handleChange={this.handleChange}
                          label='Password'
                          required />
-                    <CustomButton type="submit" > Sign In </CustomButton>
-                    <CustomButton onClick={signInWithGoogle} > Sign In with Google </CustomButton>
+                         <div className='buttons'>
+                            <CustomButton type="submit" > Sign In </CustomButton>
+                            <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In with Google </CustomButton>
+                         </div>
                 </form>
             </div>
         )
