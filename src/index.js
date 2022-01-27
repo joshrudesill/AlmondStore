b1feea5d10f7b7e54a8433f1d5de41650d9d98a5
Joshua Rudesill <joshrudesill@gmail.com>
2022-01-26 19:01:19 -0600

    added arrow functionality to checkout page, can now remove and add quantities. added and setup redux-persist

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-
+import { persistor, store } from './redux/store';
 import './index.css';
 import App from './App';
 
@@ -15,7 +14,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter> 
   </Provider>
   
