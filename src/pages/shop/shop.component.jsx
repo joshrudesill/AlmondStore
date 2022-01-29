5ff282a8dd16d559bdaf892ccd394a848e3fed83
Joshua Rudesill <joshrudesill@gmail.com>
2022-01-29 15:31:36 -0600

    added shop redux and put shop data into redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,16 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/preview-collection/collection-preview.component.jsx";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component.jsx";
+import { Route } from "react-router-dom";
+import CategoryPage from "../category/category.component.jsx";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ match }) => (
+        <div className="shop-page">
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                <Route path ={`${match.path}/:categoryId`} component={CategoryPage}/>
+        </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) =>
-                        (
-                            <CollectionPreview key={id} {... otherCollectionProps} />
-                        ))
-                }
-            </div>
-        )
-    }
-}
+
+
 
 export default ShopPage;
