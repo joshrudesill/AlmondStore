a48cf6251a5ec933d23385649220939a2d0b320c
Joshua Rudesill <joshrudesill@gmail.com>
2022-01-30 14:06:30 -0600

    bug fix redone, better solution

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-import CollectionPage from './pages/collection/collection.component';
 
 import SignInSignUpPage from './pages/signin-signup/signin-signup.component';
 import { setCurrentUser } from '../src/redux/user/user.actions';
@@ -48,10 +47,9 @@ class App extends React.Component {
         <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
-          <Route exact path='/shop' component={ShopPage}/>
+          <Route  path='/shop' component={ShopPage}/>
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInSignUpPage />)}/>
           <Route exact path='/checkout' component={CheckoutPage}/>
-          <Route path= '/shop/:collectionId' component={CollectionPage} />
         </Switch>
       </div>
     );
